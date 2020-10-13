3ae9941b26bfbef9daa1cd4ccf05f10c4b1f5110
macOS <macos@hoangquan.local>
2020-10-13 16:35:38 +0700

    add colums

---
+++ database/migrations/2020_10_13_163205_add_colum_is_run_batch.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddColumIsRunBatch extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('master_sites', function (Blueprint $table) {
+            $table->integer('is_run_batch')->default(0);
+        });
+        Schema::table('master_categories', function (Blueprint $table) {
+            $table->integer('is_run_batch')->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('master_sites', function (Blueprint $table) {
+            $table->dropColumn(['is_run_batch']);
+        });
+        Schema::table('master_categories', function (Blueprint $table) {
+            $table->dropColumn(['is_run_batch']);
+        });
+    }
+}
