242b4e6204f5e8221f89763f74780e24e238f7d7
macOS <macos@hoangquan.local>
2020-10-13 17:00:18 +0700

    Add column

--- database/migrations/2020_10_13_165700_add_column_link_mp3_to_music.php
+++ database/migrations/2020_10_13_165700_add_column_link_mp3_to_music.php
@@ -14,8 +14,8 @@ class AddColumnLinkMp3ToMusic extends Migration
     public function up()
     {
         Schema::table('music', function (Blueprint $table) {
-            $table->text('link_mp3')->default(null);
-            $table->timestamp('link_updated_at');
+            $table->text('link_mp3')->nullable()->default(null);
+            $table->timestamp('link_updated_at')->nullable();
         });
     }
 
