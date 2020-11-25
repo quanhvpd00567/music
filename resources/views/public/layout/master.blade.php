2c776b42ac8286d016de722b85a9aad9c9e6c104
macOS <macos@hoangquan.local>
2020-11-25 15:01:52 +0700

    fix meta

--- resources/views/public/layout/master.blade.php
+++ resources/views/public/layout/master.blade.php
@@ -3,13 +3,11 @@
 
 <!DOCTYPE html>
 <html lang="en">
-
 <!-- Mirrored from demo.suavedigital.com/jukebox/album-4columns.html by HTTrack Website Copier/3.x [XR&CO'2014], Wed, 04 Nov 2020 15:22:08 GMT -->
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="Jukebox - Responsive Music and Band Website Template">
     <link rel="icon" href="/vietmix/images/favicon.png">
     <base href="{{url('/')}}">
     <title>@yield('title')</title>
